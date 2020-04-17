e9202dc95d149d481b2ecc21631aebcce7af13c8
Armen <garibyan923@gmail.com>
2020-04-17 19:37:25 +0400

    google login

--- src/main/java/com/clphub/googleoauth/controller/MainController.java
+++ src/main/java/com/clphub/googleoauth/controller/MainController.java
@@ -4,16 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+public class MainController {
 
-    @GetMapping("/")
-    public String helloWorld(){
-        return "Hello World";
-    }
-
-    @GetMapping("/restricted")
+    @GetMapping("/logged")
     public String restricted(){
-        return "to see it you need to be logged in ";
+//        return "You are Logged In";
+        return "logged";
     }
 
 }
